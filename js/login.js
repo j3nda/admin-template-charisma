59663800bee6b97abc86acdef5660563e3c2234e
Jan Smid <jan@smid.sk>
2013-03-15 16:37:37 +0100

    login-amazing: fade{In|Out}

--- js/login.js
+++ js/login.js
@@ -12,15 +12,72 @@ $(window).resize(function() {
 });
 
 
+function fadeToHelper(el, index, max, timing) {
+}
+function fadeInHelper(el, index, max, timing) {
+	// TODO: respect type of message!
+	// {success|info|danger|error}
+
+	el.css('visibility', '');
+	el.fadeIn(timing['in'], function() {
+		el.fadeTo(timing['to1'][0], timing['to1'][1], function() {
+
+			if (index+1 < max) {
+				fadeInHelper(el.next(), index+1, max, timing);
+			}
+
+			el.fadeTo(timing['to2'][0], timing['to2'][1], function() {
+
+				el.fadeTo(timing['to3'][0], timing['to3'][1], function() {
+					// TODO: move to top-bar!
+					// TODO: hide
+					el.hide();
+				});
+
+			});
+
+		});
+	});
+
+	el.mouseover(function() {
+		// TODO: kdyz jsem to prerusil, tak musim zase navazat!
+		// - pamatovat -> pokud dam fadeout tak znovu fadeInHelper()
+		$(this).stop();
+		$(this).fadeTo('normal', 1.0);
+	});
+	el.mouseout(function() {
+		$(this).stop();
+		$(this).fadeTo(timing['to2'][0], timing['to2'][1], function() {
+			$(this).fadeTo(timing['to3'][0], timing['to3'][1], function() {
+				// TODO: move to top-bar!
+				// TODO: hide
+				$(this).hide();
+			});
+		});
+	});
+}
+
+
 $(document).ready(function() {
 	var backup = {
-		'login': $('fieldset #login').html()
+		'login'   : $('fieldset #login').html(),
+		'forgotem': false,//		fmsgs.each(function(index) {
+//			$(this).fadeIn(500);
+//console.log(index + ": " + $(this).text());
+		//});
 	};
 	$('.login-box').center();
 	$('.login-box').css('visibility', '');
 	$('fieldset #forgotel').css('visibility', 'hidden');
 	$('fieldset input[name=forgotemail]').attr('disabled', true);
 
+
+	// forgot password: pre-fill: username -> forgot email
+	$('fieldset input[name=username]').change(function() {
+		backup['forgotem'] = false;
+	});
+
+	// forgot password: checkbox
 	$('fieldset #forgotch').change(function() {
 		var el = {
 			'username' : $('fieldset input[name=username]'),
@@ -29,7 +86,7 @@ $(document).ready(function() {
 			'dremember': $('fieldset .remember'),
 			'forgotin' : $('fieldset input[name=forgotemail]'),
 			'forgotch' : $('fieldset #forgotch'),
-			'forgotel':  $('fieldset #forgotel'),
+			'forgotel' :  $('fieldset #forgotel'),
 			'login'    : $('fieldset #login')
 		};
 
@@ -48,6 +105,16 @@ $(document).ready(function() {
 			el['login'].html('Send');
 			el['login'].toggleClass('btn-forgot');
 
+			// pre-fill 'forgotemail' if username is valid email@address!
+			if (!backup['forgotem']) {
+				backup['forgotem'] = true;
+
+				var emailRE = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+				if (emailRE.test(el['username'].val())) {
+					el['forgotin'].val(el['username'].val());
+				}
+			}
+
 		} else {
 			el['remember'].removeAttr("disabled");
 			el['remember'].removeAttr("readonly");
@@ -64,4 +131,36 @@ $(document).ready(function() {
 			el['login'].toggleClass('btn-forgot');
 		}
 	});
+
+
+	// flash messages (~fade{In|Out})
+	var fmsg = $('#flash-messages');
+	if (fmsg.html() != '' && fmsg.html() != null) {
+		var fmsg_cnt = $('#flash-messages div');
+		var fmsg_len = fmsg_cnt.size();
+		var timing   = {
+			'in' :     777,
+			'to1': [  6666, 0.5 ],
+			'to2': [   999, 0.2 ],
+			'to3': [   555, 0.0 ]
+		};
+		fmsg_cnt.each(function(index, value) {
+			$(this).hide();
+			if (index == 0) {
+				fadeInHelper($(this), index, fmsg_len, timing);
+			}
+		});
+		fmsg.css('visibility', '');
+	}
+
+	// TODO: fadeIn (prvni)
+	// TODO: fadeOut (prvni) -> complete -> presun ji nahoru a zobraz indikator
+	// TODO: po kliku na indikator -> presun zpet a zobraz ji -> fadeIn() a opakuj akce
+
+//var fmsgs = $('#xxx');
+//fmsgs.fadeOut(2000, function() {
+//});
+
+console.log(fmsg.html());
+
 });
